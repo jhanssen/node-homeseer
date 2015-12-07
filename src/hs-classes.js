1a4f31400f165e4c38ee3c845ded46c85565f162
Jan Erik Hanssen <jhanssen@gmail.com>
2015-12-06 20:23:29 -0800

    more

--- src/hs-classes.js
+++ src/hs-classes.js
@@ -3,40 +3,95 @@
 var WebSocket = require("ws");
 
 var Homeseer = function(host) {
+    var hs = this;
     this._ws = new WebSocket(host);
-    this._ws.on('open', this._onWsOpen);
-    this._ws.on('message', this._onWsMessage);
-    this._ws.on('close', this._onWsClose);
+    this._ws.on('open', function() { hs._onWsOpen.call(hs); });
+    this._ws.on('message', function(msg, flags) { hs._onWsMessage.call(hs, msg, flags); });
+    this._ws.on('close', function() { hs._onWsClose.call(hs); });
+    this._ws.on('error', function(err) { hs._onWsError.call(hs, err); });
 };
 
 Homeseer.prototype = {
     devices: function(cb) {
-        this._request("devices", cb);
+        this._request("devices", undefined, cb);
+    },
+    on: function(type, cb) {
+        this._ons[type] = cb;
+    },
+    shutdown: function() {
+        this._ws.close();
     },
 
     _ws: undefined,
     _id: 0,
     _ons: Object.create(null),
-    _request: function(name, cb) {
+    _cbs: Object.create(null),
+    _request: function(name, args, cb) {
         var req = { type: name, id: ++this._id };
+        if (args !== undefined)
+            req.args = args;
+        this._cbs[req.id] = cb;
         this._ws.send(JSON.stringify(req));
     },
+    _callOn: function(type, arg) {
+        if (type in this._ons) {
+            var cb = this._ons[type];
+            return cb(arg);
+        }
+        return undefined;
+    },
     _onWsOpen: function() {
+        this._callOn("ready");
     },
     _onWsClose: function() {
     },
     _onWsMessage: function(msg, flags) {
         try {
-            var data = JSON.parse(msg.data);
+            var data = JSON.parse(msg);
         } catch (e) {
             console.error(e);
             return;
         }
-        console.log(data);
+        //console.log(data);
+        if (data.hasOwnProperty("id")) {
+            if (data.hasOwnProperty("type") && data.hasOwnProperty("data")) {
+                if (data.type === "request") {
+                    // special
+                    //console.log("got req", data);
+                    // var obj = {
+                    //     data: data.data
+                        // write: function(data) {
+                        //     var response = {
+                        //         id: this._id,
+                        //         type: "response",
+                        //         data: data
+                        //     };
+                        //     console.log("sending resp", response);
+                        //     this._ws.send(JSON.stringify(response));
+                        // }
+                    // };
+                    var ret = this._callOn("request", data.data);
+                    if (ret !== undefined) {
+                        var resp = { id: data.id, type: "response", data: ret };
+                        //console.log("sending resp", resp);
+                        this._ws.send(JSON.stringify(resp));
+                    }
+                    return;
+                }
+            }
+            if (data.id in this._cbs) {
+                var cb = this._cbs[data.id];
+                delete this._cbs[data.id];
+                cb(data);
+            }
+        }
+    },
+    _onWsError: function(err) {
+        console.error("WS error: " + err);
     }
 };
 
 var Device = function() {
 };
 
-module.exports = { Device: Device };
+module.exports = { Device: Device, Homeseer: Homeseer };
--- src/hs3.js
+++ src/hs3.js
@@ -2,7 +2,89 @@
 
 var classes = require("./hs-classes.js");
 
-var hs = new classes.Homeseer("ws://pi.nine.msg/homeseer");
-hs.devices(function(devs) {
-    console.log(devs);
+var hs = new classes.Homeseer("ws://pi.nine.ms:8087/homeseer");
+hs.on("ready", function() {
+    hs.devices(function(devs) {
+        console.log("DEVS", devs);
+    });
+});
+hs.on("request", function(req) {
+    var p, k, cmd = req.command;
+    if (cmd) {
+        switch (cmd.function) {
+        case "triggerCount":
+            return { value: 1};
+        case "triggerName":
+            return { value: "foobar" };
+        case "triggerConfigured":
+            var cfg = (cmd.arguments.length > 0 && cmd.arguments[0].length > 0);
+            return { value: cfg };
+        case "triggerFormatUI":
+            p = undefined;
+            try {
+                p = JSON.parse(cmd.arguments[0]);
+            } catch (e) {
+                console.log(e);
+            }
+            for (k in p) {
+                if (k.indexOf("foobarui") === 0) {
+                    return { value: p[k] };
+                }
+            }
+            console.log("no formatui for", cmd.arguments);
+            break;
+        case "triggerBuildUI":
+            p = {};
+            try {
+                if (cmd.arguments.length > 0 && cmd.arguments[0].length > 0)
+                    p = JSON.parse(cmd.arguments[0]);
+            } catch (e) {
+                console.log(e);
+            }
+            var selected;
+            for (k in p) {
+                if (k.indexOf("foobarui") === 0)
+                    selected = p[k];
+            }
+            var ui = {
+                name: "foobarui",
+                description: "Hello world:",
+                type: "jqDropList",
+                items: [
+                    { name: "--Please select--", value: "-1", selected: selected === undefined },
+                    { name: "name", value: "value", selected: selected === "value" },
+                    { name: "name2", value: "value2", selected: selected === "value2" }
+                ],
+                postback: true
+            };
+            return { value: ui };
+        case "triggerProcessPostUI":
+            console.log("post for", cmd.arguments);
+            if (cmd.arguments.length !== 2)
+                return "";
+            var prev = {};
+            if (cmd.arguments[1].length > 0) {
+                try {
+                    prev = JSON.parse(cmd.arguments[1]);
+                } catch (e) {
+                    console.error(e);
+                }
+            }
+            for (k in cmd.arguments[0]) {
+                prev[k] = cmd.arguments[0][k];
+            }
+            console.log("posted", JSON.stringify(prev));
+            return { value: JSON.stringify(prev) };
+        default:
+            console.log("unknown function " + cmd.function);
+            break;
+        }
+    }
+    //console.log(cmd);
+    return undefined;
+});
+
+process.on('SIGINT', function() {
+    hs.shutdown();
+    process.exit();
 });
